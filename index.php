baa5e5eb5f1263116899723508aca00faf023140
BiancaLouS <biancalourencosilva@gmail.com>
2023-03-01 01:14:02 -0300

    Iniciando algumas ações em PHP

--- index.php
+++ index.php
@@ -9,34 +9,11 @@
     <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
 
+    <?php include "menu_publico.php"?>
+
 </head>
 
 <body class="fundo" ng-app="">
-<!-- INICIO MENU  -->
-
-    <div>
-    
-        <ul>
-            <img style="width: 40px; margin-left: 30px;" src="images/logo.png" alt="logo">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#servicos">Serviços</a></li>
-            <li><a href="#sobre">Sobre Nós</a></li>
-            <li><a href="#contato">Contato</a></li>
-
-
-            <li class="dropdown">
-              <a href="javascript:void(0)" class="dropbtn">Produtos</a>
-              <div class="dropdown-content">
-                <a href="#">Ar Condicionado</a>
-                <a href="#">Ar Condicionado Inverter</a>
-                <a href="#">Multi Split</a>
-                <a href="#">Energia Solar</a>
-              </div>
-            </li>
-          </ul>
-    </div>
-
-<!-- FIM MENU  -->
 
 <!-- INICIO APRESENTAÇÃO INICIAL  -->
 
---
+++ menu_publico.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Publico</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <!-- INICIO MENU  -->
+
+    <div>
+    
+        <ul>
+            <img style="width: 40px; margin-left: 30px;" src="images/logo.png" alt="logo">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#servicos">Serviços</a></li>
+            <li><a href="#sobre">Sobre Nós</a></li>
+            <li><a href="#contato">Contato</a></li>
+
+
+            <li class="dropdown">
+              <a href="javascript:void(0)" class="dropbtn">Produtos</a>
+              <div class="dropdown-content">
+                <a href="#">Ar Condicionado</a>
+                <a href="#">Ar Condicionado Inverter</a>
+                <a href="#">Multi Split</a>
+                <a href="#">Energia Solar</a>
+              </div>
+            </li>
+          </ul>
+    </div>
+
+<!-- FIM MENU  -->
+</body>
+</html>
