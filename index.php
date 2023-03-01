00a83be7bc3b1da5075cae7f5e09f5e5179c2432
BiancaLouS <biancalourencosilva@gmail.com>
2023-03-01 01:22:17 -0300

    Adicionando ícone no navegador

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 
     <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    <link rel="shortcut icon" href="./images/logo.png" type="image/png">
 
     <?php include "menu_publico.php"?>
 
@@ -33,118 +34,123 @@
 
 <!-- INÍCIO VISÃO MISSÃO VALORES  -->
 
-<section>
-    
-    <div class="redirecionar">
-        <div class="redirecionar-conteudo"><strong class="txt">Missão</strong> <img class="icone" src="images/alvo.png" alt="">
+    <section>
         
-            <div class="texto2 txt">
-                <br>
-                Lorem ipsum dolor sit amet. Est dicta labore et sint expedita <br>
-                nam repellendus distinctio! Et Quis aliquam cum autem Quis ut <br>
-                voluptas blanditiis.
+        <div class="redirecionar">
+            <div class="redirecionar-conteudo"><strong class="txt">Missão</strong> <img class="icone" src="images/alvo.png" alt="">
+            
+                <div class="texto2 txt">
+                    <br>
+                    Lorem ipsum dolor sit amet. Est dicta labore et sint expedita <br>
+                    nam repellendus distinctio! Et Quis aliquam cum autem Quis ut <br>
+                    voluptas blanditiis.
+                </div>
+            </div>
+
+            <div class="redirecionar-conteudo"> <strong class="txt">Visão</strong> <img class="visao" src="images/visao.png" alt="">
+
+                <div class="texto2 txt">
+                    <br>
+                    Lorem ipsum dolor sit amet. Est dicta labore et sint expedita <br>
+                    nam repellendus distinctio! Et Quis aliquam cum autem Quis ut <br>
+                    voluptas blanditiis.
+                </div>
+            </div>
+
+            <div class="redirecionar-conteudo"> <strong class="txt">Valores</strong> <img class="icone" src="images/valor.png" alt="">
+                <div class="texto2 txt">
+                    <br>
+                    Lorem ipsum dolor sit amet. Est dicta labore et sint expedita <br>
+                    nam repellendus distinctio! Et Quis aliquam cum autem Quis ut <br>
+                    voluptas blanditiis.
+                </div>
             </div>
         </div>
 
-        <div class="redirecionar-conteudo"> <strong class="txt">Visão</strong> <img class="visao" src="images/visao.png" alt="">
-
-            <div class="texto2 txt">
-                <br>
-                Lorem ipsum dolor sit amet. Est dicta labore et sint expedita <br>
-                nam repellendus distinctio! Et Quis aliquam cum autem Quis ut <br>
-                voluptas blanditiis.
-            </div>
-        </div>
-
-        <div class="redirecionar-conteudo"> <strong class="txt">Valores</strong> <img class="icone" src="images/valor.png" alt="">
-            <div class="texto2 txt">
-                <br>
-                Lorem ipsum dolor sit amet. Est dicta labore et sint expedita <br>
-                nam repellendus distinctio! Et Quis aliquam cum autem Quis ut <br>
-                voluptas blanditiis.
-            </div>
-        </div>
-    </div>
-
-</section>
+    </section>
 
 <!-- FIM VISÃO MISSÃO VALORES  -->
 
 <!-- INICIO SERVIÇOS  -->
 
 <section class="fundo3">
-    <div>
-        <h1 class="texto3 txt2" id="servicos">
-            Serviços
-        </h1>
-        <img class="linha2" src="images/linha.svg" alt="linha bonita">
-    </div>
-
-    <div class="div-grid">
         <div>
-            <img class="img-servicos servicos-grid " src="images/InstalandoPainelSolar.jpg" alt="Instalação de painéis solares">
+            <h1 class="texto3 txt2" id="servicos">
+                Serviços
+            </h1>
+            <img class="linha2" src="images/linha.svg" alt="linha bonita">
         </div>
 
-        <div class="txt-grid txt2">
-            <h2>
-                Instalação de Painéis Solares em Indústrias
-            </h2>
-        </div>
+        <div class="div-grid">
+            <div>
+                <img class="img-servicos servicos-grid " src="images/InstalandoPainelSolar.jpg" alt="Instalação de painéis solares">
+            </div>
 
-        <div>
-            <img class="img-servicos servicos-grid " src="images/InstalandoArCond.jpg" alt="Instalação de Ar Condicionado">
-        </div>
+            <div class="txt-grid txt2">
+                <h2>
+                    Instalação de Painéis Solares em Indústrias
+                </h2>
+            </div>
 
-        <div class="txt-grid txt2"> 
-            <h2>
-                Instalação de Painés Solares em Areas Rurais
-            </h2>
-        </div>
+            <div>
+                <img class="img-servicos servicos-grid " src="images/InstalandoArCond.jpg" alt="Instalação de Ar Condicionado">
+            </div>
 
-        <div>
-            <img class="img-servicos servicos-grid " src="images/InstalandoArEmpresa.jpg" alt="Instalação de Ar Condicionado em Empresas">
-        </div>
-      
-        <div class="txt-grid txt2">
-            <h2>
-                Limpeza de Ar Condicionado
-            </h2>
-        </div>
+            <div class="txt-grid txt2"> 
+                <h2>
+                    Instalação de Painés Solares em Areas Rurais
+                </h2>
+            </div>
 
-        <div class="txt-grid txt2">
-            <h2>
-                Instalação e Manutenção de
-                     Painéis Solares
-            </h2>
-        </div>
-
-        <div class="servicos-grid">
-            <img class="img-servicos servicos-grid " src="images/PainelSolarIndustria.jpg" alt="Instalação de painéis solares">
-        </div>
+            <div>
+                <img class="img-servicos servicos-grid " src="images/InstalandoArEmpresa.jpg" alt="Instalação de Ar Condicionado em Empresas">
+            </div>
         
-        <div class="txt-grid txt2">
-            <h2>
-                Instalação de Ar Condicionado em Residências
-            </h2>
+            <div class="txt-grid txt2">
+                <h2>
+                    Limpeza de Ar Condicionado
+                </h2>
+            </div>
+
+            <div class="txt-grid txt2">
+                <h2>
+                    Instalação e Manutenção de
+                        Painéis Solares
+                </h2>
+            </div>
+
+            <div class="servicos-grid">
+                <img class="img-servicos servicos-grid " src="images/PainelSolarIndustria.jpg" alt="Instalação de painéis solares">
+            </div>
+            
+            <div class="txt-grid txt2">
+                <h2>
+                    Instalação de Ar Condicionado em Residências
+                </h2>
+            </div>
+
+            <div>
+                <img class="img-servicos servicos-grid " src="images/fotovoltaicarural.jpg" alt="Instalação de painéis solares em areas rurais">
+            </div>
+
+            <div class="txt-grid txt2">
+                <h2>
+                    Instalação e Manutenção de Ar Condicionado em Empresas
+                </h2>
+            </div>
+
+            <div>
+                <img class="img-servicos servicos-grid " src="images/limpezaar.jpg" alt="Limpeza de Ar Condicionado">
+            </div>
+
         </div>
 
-        <div>
-            <img class="img-servicos servicos-grid " src="images/fotovoltaicarural.jpg" alt="Instalação de painéis solares em areas rurais">
-        </div>
-
-        <div class="txt-grid txt2">
-            <h2>
-                Instalação e Manutenção de Ar Condicionado em Empresas
-            </h2>
-        </div>
-
-        <div>
-            <img class="img-servicos servicos-grid " src="images/limpezaar.jpg" alt="Limpeza de Ar Condicionado">
-        </div>
-
-    </div>
+    </section>
 
     <!-- FIM SERVICOS  -->
+
+<section>
+    
 
 </section>
 
